b1b7906969c8de266bd2c84196aa0f07d3c2407f
Dikla Felach <diklafelach1@gmail.com>
2024-01-26 11:02:18 +0200

    Adding 2 files to my repo

---
+++ DiklaFelach.java
@@ -0,0 +1,9 @@
+public class DiklaFelach{
+	public static void main(String [] args)
+	{
+		for(int i=0;i<2;i++)
+		{
+			System.out.println("Me name is how? \nMe name is what? \nMy name is, chka-chka, Dikla Felach");
+		}
+	}
+}
