00d3837720a8043bb0aacc5a5393cdfa071bc6a0
Dikla Felach <diklafelach1@gmail.com>
2024-01-26 11:31:19 +0200

    Adding more prints

--- DiklaFelach.java
+++ DiklaFelach.java
@@ -5,5 +5,8 @@ public class DiklaFelach{
 		{
 			System.out.println("Me name is how? \nMe name is what? \nMy name is, chka-chka, Dikla Felach");
 		}
+        System.out.println("Hi, my name is, what? (Excuse me) My name is, who?");
+        System.out.println("My name is, chka-chka, Slim Shady");
+        System.out.println("Can I have the attention of the class for one second?");
 	}
 }
